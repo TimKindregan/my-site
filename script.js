e032e6fc1c3b3b6d3bf7c176036830f403438cec
Tim Kindregan <timothyj.kindregan@gmail.com>
2022-10-08 19:19:54 -0400

    added button to show third image and resized images

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 */
+function show() {
+    document.getElementById('dog3').style.display = "block"
+}
